2c313d1098ffb1c229517fd13e3d6a4258aa1e1b
spinosaurus24 <157531624+spinosaurus24@users.noreply.github.com>
2026-04-30 00:04:13 +0200

    Update graphique.py

--- graphique.py
+++ graphique.py
@@ -1,10 +1,36 @@
 import pandas as pd
 import plotly.express as px
 
+# Création des données
 df = pd.DataFrame({
     "produit": ["A", "B", "C"],
     "total_ventes": [17500, 15825, 11500]
 })
 
-fig = px.bar(df, x="produit", y="total_ventes", title="Ventes par produit")
-fig.show()
+
+# Graphique 1 : Barres
+
+fig_bar = px.bar(
+    df,
+    x="produit",
+    y="total_ventes",
+    title="Ventes par produit"
+)
+
+# Sauvegarde du graphique
+fig_bar.write_html("ventes-barres.html")
+
+
+# Graphique 2 : Camembert
+
+fig_pie = px.pie(
+    df,
+    names="produit",
+    values="total_ventes",
+    title="Répartition des ventes"
+)
+
+# Sauvegarde du graphique
+fig_pie.write_html("ventes-camembert.html")
+
+print("Les graphiques ont été générés avec succès.")
