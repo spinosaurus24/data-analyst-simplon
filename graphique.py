50c46c815150c9a69bcbc9fa12dcd320e94f0faa
spinosaurus24 <157531624+spinosaurus24@users.noreply.github.com>
2026-04-29 23:45:45 +0200

    Update graphique.py

--- graphique.py
+++ graphique.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import plotly.express as px
 
+df = pd.DataFrame({
+    "produit": ["A", "B", "C"],
+    "total_ventes": [17500, 15825, 11500]
+})
+
+fig = px.bar(df, x="produit", y="total_ventes", title="Ventes par produit")
+fig.show()
