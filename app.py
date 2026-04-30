df5919d72649e3a5e48a5cb074bef61d03738df4
spinosaurus24 <157531624+spinosaurus24@users.noreply.github.com>
2026-04-30 12:12:46 +0000

    Rendu projet analyse ventes

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 import plotly.express as px
 import pandas as pd
 
-données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
+# Charger les données
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
-
+# Graphique existant (corrigé)
+figure = px.pie(df, values='qte', names='region', title='quantité vendue par région')
 figure.write_html('ventes-par-region.html')
 
 print('ventes-par-région.html généré avec succès !')
 
-
-ventes_produit = df.groupby("produit")["quantite"].sum().reset_index()
+# 1. VENTES PAR PRODUIT
+ventes_produit = df.groupby("produit")["qte"].sum().reset_index()
 
 fig = px.bar(
     ventes_produit,
     x="produit",
-    y="quantite",
+    y="qte",
     title="Ventes par produit"
 )
 
 fig.write_html("ventes-par-produit.html")
 
+# 2. CHIFFRE D'AFFAIRES
+df["chiffre_affaires"] = df["qte"] * df["prix"]
 
-df["chiffre_affaires"] = df["quantite"] * df["prix"]
+ca_produit = df.groupby("produit")["chiffre_affaires"].sum().reset_index()
 
 fig = px.bar(
     ca_produit,
